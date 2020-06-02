28fdfe3b4153acd05a8a0ffc27d66d659eddfa4b
Timo Paulssen <timonator@perpetuum-immobile.de>
2020-06-03 01:53:51 +0200

    log dispatch resolution and incorporate into stats
    currently looks exactly like a spesh plugin guard index in
    the stats, but there shouldn't be a conflict, i don't think.

--- src/core/interp.c
+++ src/core/interp.c
@@ -5746,11 +5746,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVMString *id = MVM_cu_string(tc, cu, GET_UI32(cur_op, 0));
                 MVMCallsite *callsite = cu->body.callsites[GET_UI16(cur_op, 4)];
                 MVMuint16 *args = (MVMuint16 *)(cur_op + 6);
+                MVMuint32 bytecode_offset = cur_op - *tc->interp_bytecode_start;
                 tc->cur_frame->return_value = NULL;
                 tc->cur_frame->return_type = MVM_RETURN_VOID;
                 cur_op += 6 + 2 * callsite->flag_count;
                 tc->cur_frame->return_address = cur_op;
-                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args);
+                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args, bytecode_offset);
                 goto NEXT;
             }
             OP(dispatch_i): {
@@ -5760,11 +5761,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVMString *id = MVM_cu_string(tc, cu, GET_UI32(cur_op, 2));
                 MVMCallsite *callsite = cu->body.callsites[GET_UI16(cur_op, 6)];
                 MVMuint16 *args = (MVMuint16 *)(cur_op + 8);
+                MVMuint32 bytecode_offset = cur_op - *tc->interp_bytecode_start;
                 tc->cur_frame->return_value = &GET_REG(cur_op, 0);
                 tc->cur_frame->return_type  = MVM_RETURN_INT;
                 cur_op += 8 + 2 * callsite->flag_count;
                 tc->cur_frame->return_address = cur_op;
-                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args);
+                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args, bytecode_offset);
                 goto NEXT;
             }
             OP(dispatch_n): {
@@ -5774,11 +5776,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVMString *id = MVM_cu_string(tc, cu, GET_UI32(cur_op, 2));
                 MVMCallsite *callsite = cu->body.callsites[GET_UI16(cur_op, 6)];
                 MVMuint16 *args = (MVMuint16 *)(cur_op + 8);
+                MVMuint32 bytecode_offset = cur_op - *tc->interp_bytecode_start;
                 tc->cur_frame->return_value = &GET_REG(cur_op, 0);
                 tc->cur_frame->return_type  = MVM_RETURN_NUM;
                 cur_op += 8 + 2 * callsite->flag_count;
                 tc->cur_frame->return_address = cur_op;
-                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args);
+                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args, bytecode_offset);
                 goto NEXT;
             }
             OP(dispatch_s): {
@@ -5788,11 +5791,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVMString *id = MVM_cu_string(tc, cu, GET_UI32(cur_op, 2));
                 MVMCallsite *callsite = cu->body.callsites[GET_UI16(cur_op, 6)];
                 MVMuint16 *args = (MVMuint16 *)(cur_op + 8);
+                MVMuint32 bytecode_offset = cur_op - *tc->interp_bytecode_start;
                 tc->cur_frame->return_value = &GET_REG(cur_op, 0);
                 tc->cur_frame->return_type  = MVM_RETURN_STR;
                 cur_op += 8 + 2 * callsite->flag_count;
                 tc->cur_frame->return_address = cur_op;
-                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args);
+                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args, bytecode_offset);
                 goto NEXT;
             }
             OP(dispatch_o): {
@@ -5802,11 +5806,12 @@ void MVM_interp_run(MVMThreadContext *tc, void (*initial_invoke)(MVMThreadContex
                 MVMString *id = MVM_cu_string(tc, cu, GET_UI32(cur_op, 2));
                 MVMCallsite *callsite = cu->body.callsites[GET_UI16(cur_op, 6)];
                 MVMuint16 *args = (MVMuint16 *)(cur_op + 8);
+                MVMuint32 bytecode_offset = cur_op - *tc->interp_bytecode_start;
                 tc->cur_frame->return_value = &GET_REG(cur_op, 0);
                 tc->cur_frame->return_type  = MVM_RETURN_OBJ;
                 cur_op += 8 + 2 * callsite->flag_count;
                 tc->cur_frame->return_address = cur_op;
-                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args);
+                ice->run_dispatch(tc, ice_ptr, ice, id, callsite, args, bytecode_offset);
                 goto NEXT;
             }
             OP(sp_guard): {
